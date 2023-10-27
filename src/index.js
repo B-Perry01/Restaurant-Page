47ca1712abe7d2345c684f4daf8575b74e201f87
Ben <benperry1915@gmail.com>
2023-10-27 16:08:53 +0100

    Add webpack production and dev environments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Development mode!');
+  }
+
 const content = document.getElementById("content");
 
 const logo = document.createElement("img");
